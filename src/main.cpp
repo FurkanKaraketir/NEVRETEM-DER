c2ecb30c80e01170dfa33920759538f14246bd95
Furkan Karaketir <furkankaraketir2005@gmail.com>
2025-12-08 19:14:41 +0300

    update system implementation

--- src/main.cpp
+++ src/main.cpp
@@ -50,12 +50,12 @@ int main(int argc, char *argv[])
     qInstallMessageHandler(messageOutput);
     
     qCInfo(configLog) << "=== NEVRETEM-DER MBS Starting ===";
-    qCInfo(configLog) << "Application version:" << "1.0.6";
+    qCInfo(configLog) << "Application version:" << "1.0.7";
     qCInfo(configLog) << "Qt version:" << QT_VERSION_STR;
     
     // Set application properties
     app.setApplicationName("NEVRETEM-DER MBS");
-    app.setApplicationVersion("1.0.6");
+    app.setApplicationVersion("1.0.7");
     app.setOrganizationName("NEVRETEM-DER");
     app.setOrganizationDomain("nevretem-der.org");
     
